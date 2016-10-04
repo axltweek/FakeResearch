a871096059819088a410d4d0feb98f8b20851644
axltweek <alexander.rykanov@p-product.com>
2016-10-04 19:31:57 +0300

    build script: add compiling

--- build.fsx
+++ build.fsx
@@ -2,10 +2,36 @@
 #r @"packages/FAKE/tools/FakeLib.dll"
 open Fake
 
-// Default target
+// Properties
+let buildDir = "./build/"
+
+// Targets
+Target "Clean" (fun _ ->
+    CleanDir buildDir
+)
+
+Target "BuildApp" (fun _ ->
+    !! "HelloWorldWebApp.sln"
+        |> MSBuildRelease buildDir "Build"
+        // |> MSBuild buildDir "Build"
+        //    [
+        //         ("Configuration", "Release")
+        //         ("Platform", "AnyCPU")
+        //         ("DeployOnBuild", "True")
+        //         ("DeployTarget", "Package")
+        //         //("OutFolder", "output")
+        //    ]
+        |> Log "AppBuild-Output: "
+)
+
 Target "Default" (fun _ ->
     trace "Hello World from FAKE"
 )
+
+// Dependencies
+"Clean"
+    ==> "BuildApp"
+    ==> "Default"
 
 // start build
 RunTargetOrDefault "Default"
